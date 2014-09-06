010ff7d2820f51e5bd4c567006f14f20b40f6474
Sherveen Mashayekhi <sherveenmashayekhi@gmail.com>
2014-09-06 18:44:30 -0400

    angular bootstrap

---
+++ app/assets/javascripts/src/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name shuffleApp
+ * @description
+ * # shuffleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('shuffleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
